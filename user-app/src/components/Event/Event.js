c6981c433994feb9dd15658cdd0b9a053dc50501
awysocka <wysocka.anna88@gmail.com>
2021-05-10 18:21:13 +0200

    Add expandable event details"

--- user-app/src/components/Event/Event.js
+++ user-app/src/components/Event/Event.js
@@ -1,11 +1,35 @@
+import React, { useState, useRef } from 'react';
+import { IoChevronDownOutline } from 'react-icons/io5';
+import { IoChevronUpOutline } from 'react-icons/io5';
 import PropTypes from 'prop-types';
+import './Event.css';
 
-const Event = ({ event: { location, title, teacher } }) => {
+const Event = ({ event: { location, title, teacher, description } }) => {
+  const [open, setOpen] = useState(false);
+  const [eventDetailsHeight, setEventDetailsHeight] = useState('0px');
+
+  const eventDetails = useRef(null);
+
+  const toggleDetails = () => {
+    setOpen(!open);
+    setEventDetailsHeight(open ? '0px' : `${eventDetails.current.scrollHeight}px`);
+  };
+
   return (
     <li>
-      <p>{location}</p>
-      <p>{title}</p>
-      <p>{teacher}</p>
+      <button onClick={toggleDetails} className={`content ${open && 'open'}`}>
+        <p>{location}</p>
+        <p>{title}</p>
+        <p>{teacher}</p>
+        {open ? <IoChevronUpOutline /> : <IoChevronDownOutline />}
+      </button>
+      <div
+        ref={eventDetails}
+        style={{ maxHeight: `${eventDetailsHeight}` }}
+        className='event-details'
+      >
+        <p>{description}</p>
+      </div>
     </li>
   );
 };
@@ -15,6 +39,7 @@ Event.propTypes = {
     location: PropTypes.string,
     title: PropTypes.string,
     teacher: PropTypes.string,
+    description: PropTypes.string,
   }),
 };
 
